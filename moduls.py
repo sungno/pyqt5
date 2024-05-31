2c06a3eb10be31fe7ec4ff65960ad997a8d51a7b
sungno <ysn3939@gmail.com>
2024-05-31 10:32:07 +0900

    first commit

--- moduls.py
+++ moduls.py
@@ -1,24 +1,12 @@
-import cx_Oracle
 import sys, types, os
 import json, time, random, re
 
 import requests
-import pandas as pd
-from bs4 import BeautifulSoup
 
-from pywinauto import findwindows
-from pywinauto import application
-
-import selenium
-from user_agents import parse
-import undetected_chromedriver as uc
-import chromedriver_autoinstaller
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.alert import Alert
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtWidgets
+from PyQt5 import uic
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QMessageBox, QFileDialog
+from PyQt5.QtGui import QIcon
+from PyQt5.Qt import *
